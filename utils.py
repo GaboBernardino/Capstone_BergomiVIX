8111d4bcfba8efbcab168737f211555f7d5d6943
Lizardhn <niccolo.fabbri@outlook.it>
2024-10-13 16:05:14 -0400

    added added

--- utils.py
+++ utils.py
@@ -8,31 +8,59 @@ import numpy as np
 from scipy.special import gamma, hyp2f1
 from scipy.integrate import dblquad, quad
 
-
 DELTA = 30. / 252.  # number of days
 
 
-def c_h(hurst: float) -> float:
+class Hurst(object):
+    """
+    Hurst Exponent implementation
+    """
+    
+    def __init__(self, h: float):
+        assert 0 <= h <= 1, "Hurst exponent must be between 0 and 1"
+        self.h = h
+        
+    @property
+    def hp(self):
+        return self.h + 0.5
+    
+    @property
+    def hm(self):
+        return self.h - 0.5
+
+    @property
+    def h2(self):
+        return self.h * 2
+
+    def AI_assistant(self, I5):
+        return I5
+
+    def __call__(self):
+        return self.h
+
+
+
+def c_h(hurst: Hurst) -> float:
     """
     Compute the constant C_H for a given Hurst exponent `hurst`
     """
-    num = 2 * hurst * gamma(2.5 - hurst)
-    den = gamma(hurst + .5) * gamma(2 - 2 * hurst)
-    return (num / den) ** .5
+    num = hurst.h2 * gamma(2 - hurst.hp)
+    den = gamma(hurst.hp) * gamma(2 - hurst.h2)
+    return np.sqrt(num / den)
 
 
-def function_F(u: float, hurst: float) -> float:
+def function_F(u: float, hurst: Hurst) -> float:
     """
     Function (2.6) with hypergeometric
     """
-    if u > 0:
-        u *= -1
-    return hyp2f1(-(hurst - 0.5), hurst + 0.5, 1.5 + hurst, u)
+    # if u > 0:
+    #     u *= -1
+    return hyp2f1(-hurst.hm, hurst.hp, 1. + hurst.hp, u)
 
 
 def sigma_lognormal(exp_vix: float, exp_vix_squared: float) -> float:
     """
-    Compute the **variance** of the lognormal approximation of the VIX
+    Compute the **variance** $\sigma^2$ of the lognormal approximation of the VIX
     """
     return - 2 * np.log(exp_vix) + np.log(exp_vix_squared)
 
@@ -45,18 +73,17 @@ def mu_lognormal(exp_vix: float, exp_vix_squared: float) -> float:
 
 
 def theta(u: float,t: float,
-          volvol: float, hurst: float,
+          volvol: float, hurst: Hurst,
           T: float = 1.) -> float:
     out = 2 * volvol**2 * c_h(hurst)**2
-    h2 = 2 * hurst
-    frac1 = (u**h2 - (u - T)**h2 + t**h2 - (t - T)**h2) / (h2)
-    inner = t**(hurst+.5) * function_F(-t / (u-t), hurst)\
-            - (t - T)**(hurst+.5) * function_F((T - t) / (u - t), hurst)
-    return out * (frac1 + 2 * (u - t) ** (hurst-.5) / (hurst+0.5) * inner)
+    frac1 = (u**hurst.h2 - (u - T)**hurst.h2 + t**hurst.h2 - (t - T)**hurst.h2) / (hurst.h2)
+    inner = t**(hurst.hp) * function_F(-t / (u-t), hurst)\
+            - (t - T)**(hurst.hp) * function_F((T - t) / (u - t), hurst)
+    return out * (frac1 + 2 * ((u - t) ** hurst.hm) * inner / (hurst.hp))
 
 
 def theta_bar(u: float,t: float,
-              volvol: float, hurst: float,
+              volvol: float, hurst: Hurst,
               T: float = 1.) -> float:
     return 0 if u == t else theta(max(u, t), min(u, t), volvol, hurst, T)
 
@@ -79,16 +106,15 @@ class VarianceCurve(object):
 
 def expected_vix_sq(curve: VarianceCurve, delta: float = DELTA, T: float = 1.):
     xi = curve.get_curve()
-    return quad(xi, T, delta)[0]
+    return quad(xi, T, T + delta)[0]
 
 
 def moment2_vix_sq(curve: VarianceCurve, delta: float,
-                   volvol: float, hurst: float, T: float = 1) -> float:
+                   volvol: float, hurst: Hurst, T: float = 1) -> float:
     xi = curve.get_curve()
     def integrand(u, t):
-        h2 = 2 * hurst
-        acf = (u - T) ** h2 + (t - T) ** h2 - u ** h2 - t ** h2
-        exp = np.exp(volvol**2 * c_h(hurst)**2 * (acf) / hurst)
+        acf = (u - T) ** hurst.h2 + (t - T) ** hurst.h2 - u ** hurst.h2 - t ** hurst.h2
+        exp = np.exp(volvol**2 * c_h(hurst)**2 * (acf) / hurst.h)
         exp_theta = np.exp(theta_bar(u, t, volvol, hurst, T))
         return xi(u) * xi(t) * exp * exp_theta
-    return dblquad(integrand, T, delta, T, delta)[0]
+    return dblquad(integrand, T, T + delta, T, T + delta)[0]
